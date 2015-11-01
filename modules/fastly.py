07ca1839a04fb245e1259f522ce5e3a53a670aec
Justin Holmes <justin@nascency.co.uk>
2015-11-01 00:30:02 +0000

    Added purge by url

--- modules/fastly.py
+++ modules/fastly.py
@@ -29,6 +29,14 @@ EXAMPLES = '''
       service: "SU1Z0isxPaozGVKXdv0eY"
       command: "purge_all"
 
+    - local_action:
+        module: fastly
+        key: "d3cafb4dde4dbeef"
+        service: "SU1Z0isxPaozGVKXdv0eY"
+        command: "purge_url"
+        purge_url_path: "/image.jpg"
+        purge_url_host: "www.example.com"
+
 '''
 
 import sys
@@ -38,13 +46,17 @@ def main():
     argument_spec=dict(
         key=dict(type='str', required=True),
         service=dict(type='str', required=True),
-        command=dict(type='str', required=True)
+        command=dict(type='str', required=True),
+        purge_url_path=dict(type='str'),
+        purge_url_host=dict(type='str')
     )
     module = AnsibleModule(argument_spec, supports_check_mode=False)
 
     key = module.params.get('key')
     service = module.params.get('service')
     command = module.params.get('command')
+    purge_url_path = module.params.get('purge_url_path')
+    purge_url_host = module.params.get('purge_url_host')
 
     api = fastly.API()
     api.authenticate_by_key(key)
@@ -54,6 +66,10 @@ def main():
         module.exit_json(changed=apicall)
         sys.exit(0)
 
+    if command == 'purge_url':
+        apicall = api.purge_url(purge_url_host, purge_url_path)
+        module.exit_json(changed=apicall)
+        sys.exit(0)
 
 # import module snippets
 from ansible.module_utils.basic import *
